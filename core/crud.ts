944bd852eecc889594fca824ab36a8e2383b23f7
Ronan Avila <avila.ronan@gmail.com>
2024-02-07 20:26:00 -0300

    next-install

--- core/crud.ts
+++ core/crud.ts
@@ -1,15 +1,19 @@
 import fs from "fs";
+import { v4 as uuid } from "uuid";
 
 const DB_PATH = "./core/db";
 
+type UUID = string;
 interface Todo {
+    uid: UUID,
     date: string;
     content: string;
     done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        uid: uuid(),
         date: new Date().toISOString(),
         content: content,
         done: false
@@ -17,6 +21,8 @@ function create(content: string) {
 
     const todos: Todo[] = [...read(), todo]
     fs.writeFileSync(DB_PATH, JSON.stringify({ todos }, null, 2))
+
+    return todo;
 }
 
 function read(): Array<Todo> {
@@ -29,6 +35,40 @@ function read(): Array<Todo> {
     return db.todos;
 }
 
+function update(uid: UUID, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo;
+    const todos = read();
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.uid === uid;
+        if (isToUpdate) {
+            updatedTodo = Object.assign(currentTodo, partialTodo)
+        }
+    });
+
+    fs.writeFileSync(DB_PATH, JSON.stringify({ todos }, null, 2))
+    if (!updatedTodo) {
+        throw new Error("Error")
+    }
+    return updatedTodo;
+}
+function updateContentById(uid: UUID, content: string): Todo {
+    return update(uid, { content })
+}
+
+function deleteById(uid: UUID) {
+    const todos = read();
+
+    const todosWithoutOne = todos.filter((todo) => {
+        if (todo.uid === uid)
+            return false;
+
+        return true;
+
+    })
+
+    fs.writeFileSync(DB_PATH, JSON.stringify({ todos: todosWithoutOne }, null, 2))
+}
+
 function CLEAR_DB() {
     fs.writeFileSync(DB_PATH, "")
 }
@@ -36,7 +76,12 @@ function CLEAR_DB() {
 CLEAR_DB()
 create("asd")
 create("asd")
-create("asd")
+const terceira = create("asd");
+const quarta = create("asd");
+
+update(terceira.uid, { content: "aa", done: true })
+updateContentById(quarta.uid, "Batman")
+deleteById(quarta.uid)
 
 
 console.log(read())
